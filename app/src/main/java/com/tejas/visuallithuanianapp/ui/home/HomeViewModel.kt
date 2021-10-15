058efb3f8ec8e5c4ea8ed719bc8427a5774bd1ea
Tejas <tejaskhartude@gmail.com>
2021-10-15 13:49:19 +0530

    Initial commit

---
+++ app/src/main/java/com/tejas/visuallithuanianapp/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package com.tejas.visuallithuanianapp.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
